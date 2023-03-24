c56732b92627a74cb8863cb7c5c969138667827a
Aliwoto <woto@kaizoku.cyou>
2023-03-24 21:53:33 +0330

    Fix Upgrade.bat file.
    Signed-off-by: Aliwoto <woto@kaizoku.cyou>

--- Upgrade.bat
+++ Upgrade.bat
@@ -1,3 +1,3 @@
 @echo off
-git pull && powershell -command "Stop-service -Force -name "PsstRobot" -ErrorAction SilentlyContinue; go mod tidy; go build; Start-service -name "PsstRobot""
+git pull && powershell -command "Stop-service -Force -name "AnonimasuRobot" -ErrorAction SilentlyContinue; go mod tidy; go build; Start-service -name "AnonimasuRobot""
 :: Hail Hydra
